99fb498950b926a286ed26684c4c02426328104b
Keanu Valencia <104657054+keanudv@users.noreply.github.com>
2024-07-01 19:48:32 -1000

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -10,6 +10,7 @@ import seaborn as sns
 
 # Load the data set into Python
 file_location = "C:\\Users\\keanu\\OneDrive\\Desktop\\Hotsheet_spreadsheet_version.csv"
+df = pd.read_csv(file_location, encoding="UTF-8")
 
 '''
 Note that you have to use double backslashes (\\) for the Pandas read_csv() function to work properly.
@@ -17,38 +18,34 @@ Also, you must have the data set in CSV format for the Pandas read_csv() functio
 Also, know what character encoding the data set is using. For my project, the data set is in CSV UTF-8.
 '''
 
-df = pd.read_csv(file_location, encoding="UTF-8")
-
 # View the data
 df.head(10)
 df.tail(10)
 
-# Initial Exploratory Data Analysis (EDA)
+# Exploratory Data Analysis (EDA)
 df.shape #The shape function shows how many rows and columns the data set has.
-
-df.info() #The info function shows the data type of each column.
-
-df.describe() #The describe function shows summary statistics for numerical columns/quantitative data.
+df.info() #The info() function shows the data type of each column.
+df.describe() #The describe() function shows summary statistics for numerical columns/quantitative data.
 
 '''
-Upon analyzing the data type, it seems that Python is not interpreting the Original Price and Price columns as quantitative data.
-The reason is because the Original Price and Price column is formatted in Accouting ($ at the beginning).
+Upon analyzing the data types using the describe() function, it shows that Python is not interpreting the Original Price and Price columns as quantitative data.
+The reason is because the Original Price and Price column is formatted in Accouting ($) on Excel.
 Python interperts the $ as a String data type.
-Therefore, to fix this issue, I have to adjust the formatting in Excel.
+To fix this issue, I have to adjust the formatting in Excel.
+Once the formatting is fixed, I can use the info() function to see if Python now interprerts the Original Price and Price columns as quantitative data.
 '''
 
-# Once the formatting is adjusted, I can use the info function to see if Python interprets the Original Price and Price columns as quantitative data (int or float)
 df.info()
 
-# Now, I can get summary statistics on the Original Price and Price columns
+# Get summary statistics on the Original Price and Price columns
 df.describe()
 
 # Check for missing values
 df.isnull().sum()
 
 '''
-The Price column has 4 nulls.
-Because I am going to do an analysis on the Price column, I have to dig deep into why the values are missing. 
+Upon analyzing the missing values using the isnull().sum() function, it shows that the Price column has 4 missing values.
+Because I am going to do an analysis on the Price column, I have to dig deep into why the values are missing.
 '''
 
 print(df[df["Price"].isnull()]) #This code shows the rows of the Price column that contain missing values.
